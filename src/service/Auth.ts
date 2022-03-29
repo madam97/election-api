680921d8262b4e792cf2b4b275ad62d84ba48186
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-29 16:17:39 +0200

    renamed services directory to service changed Auth to use methods apart to generate access and refresh token, verifyPassword doesn't throw error

--- src/service/Auth.ts
+++ src/service/Auth.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { authConfig } from '../config';
 import IObject from '../interfaces/IObject';
@@ -11,8 +11,18 @@ export interface AuthUser {
   role?: TAuthRole
 };
 
-export interface AuthPayload {
+export interface TokenPayload {
   user: AuthUser
+};
+
+export interface AuthCitizen {
+  id: number,
+  firstname: string,
+  lastname: string
+}
+
+export interface RefreshTokenPayload {
+  user: AuthCitizen
 };
 
 export class Auth {
@@ -39,39 +49,78 @@ export class Auth {
    * Compares the given password and hash using Bcrypt
    * @param password 
    * @param hash 
+   * @returns 
    */
-  public async verifyPassword(password: string, hash: string): Promise<void> {
-    if (! await bcrypt.compare(password, hash) ) {
-      throw new Error('password is invalid');
-    }
+  public async verifyPassword(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
   }
 
   /**
-   * Generates a JWT access or refresh token using the given payload data
+   * Generates a JWT access token using the given payload data
    * @param user
-   * @param accessToken
+   * @param signOptions
    * @returns 
    */
-  public getToken(user: AuthUser, accessToken: boolean = true): string {
-    const payload: AuthPayload = {
+  public getToken(user: AuthUser, signOptions: SignOptions = {}): string {
+    const payload: TokenPayload = {
       user
     };
 
-    return accessToken ?
-      jwt.sign(payload, this.secret, { expiresIn: '1h' }) :
-      jwt.sign(payload, this.refreshTokenSecret, { expiresIn: '1d' });
+    signOptions = {
+      ...{
+        expiresIn: '1h'
+      },
+      ...signOptions
+    }
+
+    return jwt.sign(payload, this.secret, signOptions);
   }
 
   /**
-   * Verifies the given JWT access or refresh token and returns its payload
-   * @param token 
-   * @param accessToken 
+   * Generates a JWT refresh token using the given payload data
+   * @param user
+   * @param signOptions
    * @returns 
    */
-  public verifyToken(token: string, accessToken: boolean = true): AuthPayload {
-    const payload = accessToken ?
-      jwt.verify(token, this.secret) :
-      jwt.verify(token, this.refreshTokenSecret);
+  public getRefreshToken(user: AuthCitizen, signOptions: SignOptions = {}): string {
+    const payload: RefreshTokenPayload = {
+      user
+    };
+
+    signOptions = {
+      ...{
+        expiresIn: '1m'
+      },
+      ...signOptions
+    }
+
+    return jwt.sign(payload, this.refreshTokenSecret, signOptions);
+  }
+
+  /**
+   * Verifies the given JWT access token and returns its payload
+   * @param token 
+   * @returns 
+   */
+  public verifyToken(token: string): TokenPayload {
+    const payload = jwt.verify(token, this.secret);
+
+    if (typeof payload === 'string' || !payload.user) {
+      throw new Error('token payload is invalid');
+    }
+
+    return { 
+      user: payload.user
+    };
+  }
+
+  /**
+   * Verifies the given JWT refresh token and returns its payload
+   * @param token 
+   * @returns 
+   */
+  public verifyRefreshToken(token: string): RefreshTokenPayload {
+    const payload = jwt.verify(token, this.refreshTokenSecret);
 
     if (typeof payload === 'string' || !payload.user) {
       throw new Error('token payload is invalid');
@@ -87,7 +136,7 @@ export class Auth {
    * @param headers 
    * @returns 
    */
-  public verifyAuth(headers: IObject): AuthPayload {
+  public verifyAuth(headers: IObject): TokenPayload {
     if (!headers['authorization']) {
       throw new Error('authorization header is missing');
     }
