c41cdb8241f3f226348827a56de9e10795332bcf
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-29 18:30:24 +0200

    added findByDistrict method to Candidate repository

--- src/repository/CandidateRepository.ts
+++ src/repository/CandidateRepository.ts
@@ -1,4 +1,13 @@
 import { AppDataSource } from '../data-source';
 import { Candidate } from '../entity/Candidate';
+import { District } from '../entity/District';
 
-export const candidateRepository = AppDataSource.getRepository(Candidate);
+export const candidateRepository = AppDataSource.getRepository(Candidate).extend({
+  async findByDistrict(district: District): Promise<Candidate[]> {
+    return this.createQueryBuilder('candidate')
+      .innerJoinAndSelect('candidate.party', 'party')
+      .innerJoinAndSelect('candidate.district', 'district')
+      .where('district.id = :districtId', { districtId: district.id })
+      .getMany()
+  }
+});
